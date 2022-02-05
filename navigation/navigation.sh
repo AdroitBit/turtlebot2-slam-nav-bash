d4b0fde989abdd3b43f1c3249a8c2b1e722d995d
VeryHardBit <yanothaic8@gmail.com>
2022-02-05 16:16:51 +0700

    yum yum

--- navigation/navigation.sh
+++ navigation/navigation.sh
@@ -5,3 +5,13 @@ export TURTLEBOT_GAZEBO_MAP_FILE=MAPS_DIR/map.yaml
 
 
 roslaunch turtlebot_rviz_launchers view_navigation.launch
+
+: "
+So many sussy things going on
+Should you run navigation node before this rviz launcher?
+If so which one?
+
+turtlebot_gazebo amcl_demo.launch run turtlebot_navigation move_base.launch.xml
+
+This is too hard let's ask senior
+"
